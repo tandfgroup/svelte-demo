e3de2fc2ad49e34e407afd891e41efd9156760a8
guptashi <95851691+guptshi@users.noreply.github.com>
2022-05-09 17:01:59 +0530

    changes done

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,14 @@
 <script>
+
   import { Col, Container, Row } from 'sveltestrap';
+	
 </script>
 <Container lg>
 	<crud-element>
 		Basic Crud Element
 	</crud-element>
+	<tnf-list-ce/>
+	<tnf-button-ce/>
 </Container>
 
 <style>
