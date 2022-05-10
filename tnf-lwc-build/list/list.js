7919f6b9db5cdb7b5e9966641e271cc254038a26
tnf-shivanggupta <95851691+tnf-shivanggupta@users.noreply.github.com>
2022-05-10 09:45:52 +0000

    Deploying to gh-pages from @ tandfgroup/svelte-demo@a66bc674d2de0520bd178f92841509950e90cf08 🚀

--- tnf-lwc-build/list/list.js
+++ tnf-lwc-build/list/list.js
@@ -1,1 +1,79 @@
-!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t(require("lwc")):"function"==typeof define&&define.amd?define(["lwc"],t):(e="undefined"!=typeof globalThis?globalThis:e||self).list=t(e.lwc)}(this,(function(e){"use strict";var t=[function(e,t,s){var n=e?"["+e+"]":"";return"main"+n+" {margin: 30px;display: flex;flex-direction: column;align-items: center;}h1"+n+" {color: #1798c1;}"}];const s={key:0},n={attrs:{src:"./recipes-logo.png",alt:"logo"},key:1},l={key:2};function o(e,t,o,i){const{h:r,d:a,t:u,b:c}=e,{_m0:m}=i;return[r("main",s,[r("img",n),r("h1",l,[u(a(t.message))]),r("input",{props:{value:t.message},key:3,on:{input:m||(i._m0=c(t.handleInput))}})])]}var i=e.registerTemplate(o);o.stylesheets=[],t&&o.stylesheets.push.apply(o.stylesheets,t),t&&(o.stylesheetToken="tnf-list_list");class r extends e.LightningElement{constructor(...e){super(...e),this.message="Hello World"}handleInput(e){this.message=e.target.value}}e.registerDecorators(r,{fields:["message"]});var a=e.registerComponent(r,{tmpl:i});return customElements.define("tnf-list-ce",r.CustomElementConstructor),a}));
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory(require('lwc')) :
+    typeof define === 'function' && define.amd ? define(['lwc'], factory) :
+    (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.list = factory(global.lwc));
+})(this, (function (lwc) { 'use strict';
+
+    function stylesheet(token, useActualHostSelector, useNativeDirPseudoclass) {
+      var shadowSelector = token ? ("[" + token + "]") : "";
+      return "main" + shadowSelector + " {margin: 30px;display: flex;flex-direction: column;align-items: center;}h1" + shadowSelector + " {color: #1798c1;}";
+      /*LWC compiler v2.13.3*/
+    }
+    var _implicitStylesheets = [stylesheet];
+
+    var _implicitScopedStylesheets = undefined;
+
+    const stc0 = {
+      key: 0
+    };
+    const stc1 = {
+      key: 1
+    };
+    function tmpl($api, $cmp, $slotset, $ctx) {
+      const {d: api_dynamic_text, t: api_text, h: api_element, b: api_bind} = $api;
+      const {_m0} = $ctx;
+      return [api_element("main", stc0, [api_element("h1", stc1, [api_text(api_dynamic_text($cmp.message))]), api_element("input", {
+        props: {
+          "value": $cmp.message
+        },
+        key: 2,
+        on: {
+          "input": _m0 || ($ctx._m0 = api_bind($cmp.handleInput))
+        }
+      })])];
+      /*LWC compiler v2.13.3*/
+    }
+    var _tmpl = lwc.registerTemplate(tmpl);
+    tmpl.stylesheets = [];
+
+
+    if (_implicitStylesheets) {
+      tmpl.stylesheets.push.apply(tmpl.stylesheets, _implicitStylesheets);
+    }
+    if (_implicitStylesheets || _implicitScopedStylesheets) {
+      tmpl.stylesheetToken = "tnf-list_list";
+    }
+
+    const registerCustomComponent = (name, ctor) => {
+      if (!customElements.get(name)) {
+        customElements.define(name, ctor);
+      }
+    };
+
+    class List extends lwc.LightningElement {
+      constructor(...args) {
+        super(...args);
+        this.message = "Hello World";
+      }
+
+      handleInput(event) {
+        this.message = event.target.value;
+      }
+      /*LWC compiler v2.13.3*/
+
+
+    }
+
+    lwc.registerDecorators(List, {
+      fields: ["message"]
+    });
+
+    var list = lwc.registerComponent(List, {
+      tmpl: _tmpl
+    }); // Don't use the same name as tnf-list. It will break the functionality during composition
+
+    registerCustomComponent('tnf-list-ce', List.CustomElementConstructor);
+
+    return list;
+
+}));
